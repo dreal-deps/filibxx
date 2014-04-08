b6a21dfaa124e81c76bd672fdf8eae3acfd6e5f0
Soonho Kong <soonhok@cs.cmu.edu>
2014-04-08 11:39:48 -0400

    fix(test/makeTestCases.cpp): out-of-bound

--- test/makeTestCases.cpp
+++ test/makeTestCases.cpp
@@ -1,15 +1,15 @@
-/*                                                                           
-**  fi_lib++  --- A fast interval library (Version 2.0)                     
-**                                                                  
-**  Copyright (C) 2001:                                                        
-**                                                     
-**  Werner Hofschuster, Walter Kraemer                               
-**  Wissenschaftliches Rechnen/Softwaretechnologie (WRSWT)  
-**  Universitaet Wuppertal, Germany                                           
-**  Michael Lerch, German Tischler, Juergen Wolff von Gudenberg       
-**  Institut fuer Informatik                                         
-**  Universitaet Wuerzburg, Germany                                           
-** 
+/*
+**  fi_lib++  --- A fast interval library (Version 2.0)
+**
+**  Copyright (C) 2001:
+**
+**  Werner Hofschuster, Walter Kraemer
+**  Wissenschaftliches Rechnen/Softwaretechnologie (WRSWT)
+**  Universitaet Wuppertal, Germany
+**  Michael Lerch, German Tischler, Juergen Wolff von Gudenberg
+**  Institut fuer Informatik
+**  Universitaet Wuerzburg, Germany
+**
 **  This library is free software; you can redistribute it and/or
 **  modify it under the terms of the GNU Library General Public
 **  License as published by the Free Software Foundation; either
@@ -42,137 +42,137 @@ typedef union { double r; char a[7]; } twoint;
 twoint trick;
 
 #if (BYTE_ORDER == LITTLE_ENDIAN)
-	int wks = 2;
+        int wks = 2;
 #else
-	int wks = 1;
-#endif 
+        int wks = 1;
+#endif
 
 int alltrue=1;
 
 char hexr(char const *b,int const & i)
 {
-	int j;
-	int k;
-	char erg, rueck;
+        int j;
+        int k;
+        char erg, rueck;
 
-	erg=*(b+(2*i-2));
+        erg=*(b+(2*i-2));
 
-	switch (erg)
-	{ 
-		case 'A': k=10; break;
-		case 'B': k=11; break;
-		case 'C': k=12; break;
-		case 'D': k=13; break;
-		case 'E': k=14; break;
-		case 'F': k=15; break;
-		default:k=erg-48;
-	}
+        switch (erg)
+        {
+                case 'A': k=10; break;
+                case 'B': k=11; break;
+                case 'C': k=12; break;
+                case 'D': k=13; break;
+                case 'E': k=14; break;
+                case 'F': k=15; break;
+                default:k=erg-48;
+        }
 
-	k=k*16;
-	erg=*(b+(2*i-1));
+        k=k*16;
+        erg=*(b+(2*i-1));
 
-	switch (erg)
-	{
-		case 'A': j=10; break;
-		case 'B': j=11; break;
-		case 'C': j=12; break;
-		case 'D': j=13; break;
-		case 'E': j=14; break;
-		case 'F': j=15; break;
-		default:j=erg-48;
-	}
+        switch (erg)
+        {
+                case 'A': j=10; break;
+                case 'B': j=11; break;
+                case 'C': j=12; break;
+                case 'D': j=13; break;
+                case 'E': j=14; break;
+                case 'F': j=15; break;
+                default:j=erg-48;
+        }
 
-	k=k+j;
-	rueck=k;
-	return rueck;
+        k=k+j;
+        rueck=k;
+        return rueck;
 }
 
 double hexu(char * const b)
 {
-	int i;
+        int i;
 
-	for (i=1;i<=8;i++) 
-	{
-		if (wks==1) trick.a[i-1]=hexr(b,i);
-		if (wks==2) trick.a[i-1]=hexr(b,9-i);
-	}
+        for (i=1;i<8;i++)
+        {
+                if (wks==1) trick.a[i-1]=hexr(b,i);
+                if (wks==2) trick.a[i-1]=hexr(b,9-i);
+        }
 
-	return trick.r;
+        return trick.r;
 }
 
 #include "makeTestCases.icc"
 
 void executeTest()
 {
-	if (!filib::interval<double,filib::native_switched,filib::i_mode_extended>::isExtended()) 
-	{
-		std::cout << "ERROR: makeTestCases must be run in extended mode !" << std::endl;
-		exit(-1);
-	}
-  
-	filib::interval<double,filib::native_switched,filib::i_mode_extended>::precision(10);
+        if (!filib::interval<double,filib::native_switched,filib::i_mode_extended>::isExtended())
+        {
+                std::cout << "ERROR: makeTestCases must be run in extended mode !" << std::endl;
+                exit(-1);
+        }
 
-	std::ofstream out;
-	out.open("testSet.cpp");
-	out << "#include \"config.h\"" << std::endl;
-	out << "#include \"tsPredSucc.cpp\"" << std::endl;
-	out << "#include \"tsBounds.cpp\"" << std::endl;
-	out << "#include \"tsInfo.cpp\"" << std::endl;
-	out << "#include \"tsUtil.cpp\"" << std::endl;
-	out << "#include \"tsSetOp.cpp\"" << std::endl;
-	out << "#include \"tsAri.cpp\"" << std::endl;
-	out << "#include \"tsStdFun.cpp\"" << std::endl;
-	out << std::endl;
+        filib::interval<double,filib::native_switched,filib::i_mode_extended>::precision(10);
 
-	size_t n = commonIntvTestSet_size();
+        std::ofstream out;
+        out.open("testSet.cpp");
+        out << "#include \"config.h\"" << std::endl;
+        out << "#include \"tsPredSucc.cpp\"" << std::endl;
+        out << "#include \"tsBounds.cpp\"" << std::endl;
+        out << "#include \"tsInfo.cpp\"" << std::endl;
+        out << "#include \"tsUtil.cpp\"" << std::endl;
+        out << "#include \"tsSetOp.cpp\"" << std::endl;
+        out << "#include \"tsAri.cpp\"" << std::endl;
+        out << "#include \"tsStdFun.cpp\"" << std::endl;
+        out << std::endl;
 
-	printTestSetSize("ci", n, citsExtendedCases, out);
+        size_t n = commonIntvTestSet_size();
 
-	printTestSet("ciTestSet", n, 
-		citsExtendedCases, 
-		commonIntvTestSet, out);
-	
-	n = setRelTestSet1_size();
+        printTestSetSize("ci", n, citsExtendedCases, out);
 
-	printTestSetSize("sr", n, srtsExtendedCases, out);
-	printTestSet("srTestSet1", n, srtsExtendedCases, setRelTestSet1, out);
-	printTestSet("srTestSet2", n, srtsExtendedCases, setRelTestSet2, out);
+        printTestSet("ciTestSet", n,
+                citsExtendedCases,
+                commonIntvTestSet, out);
 
-	out.close();
+        n = setRelTestSet1_size();
 
-	out.open("tsPredSucc.cpp");
-	makePredSuccTestSet(out);
-	out.close();
+        printTestSetSize("sr", n, srtsExtendedCases, out);
+        printTestSet("srTestSet1", n, srtsExtendedCases, setRelTestSet1, out);
+        printTestSet("srTestSet2", n, srtsExtendedCases, setRelTestSet2, out);
 
-	out.open("tsBounds.cpp");
-	makeBoundsTestSet(out);  
-	out.close();
+        out.close();
 
-	out.open("tsInfo.cpp");
-	makeInfoTestSet(out);
-	out.close();
+        out.open("tsPredSucc.cpp");
+        makePredSuccTestSet(out);
+        out.close();
 
-	out.open("tsUtil.cpp");
-	makeUtilTestSet(out);  
-	out.close();
+        out.open("tsBounds.cpp");
+        makeBoundsTestSet(out);
+        out.close();
 
-	out.open("tsSetOp.cpp");
-	makeSetOpTestSet(out);  
-	out.close();
+        out.open("tsInfo.cpp");
+        makeInfoTestSet(out);
+        out.close();
 
-	out.open("tsAri.cpp");
-	makeAriTestSet(out);  
-	out.close();
+        out.open("tsUtil.cpp");
+        makeUtilTestSet(out);
+        out.close();
 
-	out.open("tsStdFun.cpp");
-	makeStdFunTestSet(out);  
-	out.close();
+        out.open("tsSetOp.cpp");
+        makeSetOpTestSet(out);
+        out.close();
+
+        out.open("tsAri.cpp");
+        makeAriTestSet(out);
+        out.close();
+
+        out.open("tsStdFun.cpp");
+        makeStdFunTestSet(out);
+        out.close();
 }
 
-int main() 
+int main()
 {
-	filib::fp_traits<double,filib::native_switched>::setup();
-	setup_sets();
-	executeTest();
-	return 0;
+        filib::fp_traits<double,filib::native_switched>::setup();
+        setup_sets();
+        executeTest();
+        return 0;
 }
